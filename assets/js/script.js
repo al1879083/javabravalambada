52210a3c7f5c22912e278914e37796c2427fc63e
Adrian <adrianlandag@gmail.com>
2021-03-07 21:26:46 -0800

    final edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
-//var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'];
 var lower = 'abcdefghijklmnopqrstuvwxyz';
-//var numberCase = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var number = '0123456789'
-//var specialSymbol= ['@', '$', '%', '^', '*', '!', '?', '+', '#', '&', '`', '~', '(', ')', '{', '}', '.'];
 var symbol = '!@#$%^&*()_'
 
 // Pool of all usable characters
@@ -12,27 +8,33 @@ var characterPool = '';
 
 var specialCharacters = '';
 var numberValue = '';
-var password = 'h';
+var password = '';
 var passwordText = document.querySelector ("#password");
 
 // Function used to generate password
 function  generatePassword () {
+    password = ' ';
     // Determine length of password from user
    var passLength = quantityCharacters();
 
    // Determine the avaiable pool of characters
-   
-   // Loop however many times the user inputs.
-   for (i = 0; i < passLength - 1; i++){
-       var random = Math.floor(Math.random() * characterPool.length);
-       password += characterPool[random];
+   // Keep checking until there is a valid pool.
+   while(characterPool == ''){
+       addPool();
    }
+   
+   // Loop however many times the user input.
+   for (i = 0; i < passLength - 1; i++){
+       // Add on a random index from the characterPool string.
+       password += characterPool[Math.floor(Math.random() * characterPool.length)];
+   }
+   // Return the password that was just made.
    return password;
 
 }
 
 function quantityCharacters (){
-    specialCharacters = prompt ("Please enter a number between '8' and '128'");
+    var specialCharacters = prompt ("Please enter a number between '8' and '128'");
     console.log(parseInt(specialCharacters));
     if (specialCharacters >= 8 && specialCharacters <= 128) {
         //passwordOptions ();
@@ -43,44 +45,60 @@ function quantityCharacters (){
         quantityCharacters();
     }
 }
+function addPool(){
+    // Check if user wants lowercase in their password.
+    lowerPool();
+    // Check if user wants uppercase in their password.
+    upperPool();
+    // Check if user wants numbers in their password.
+    numberPool();
+    // Check if user wants symbols in their password.
+    symbolPool();
+
+    if (characterPool == ''){
+        alert("Need atleast one character.")
+    }
+}
 function lowerPool(){
-    lowerPool = prompt ("Do you want lowercase letters? [y/n]")
+    // Prompt for character pool.
+    var lowerPool = prompt ("Do you want lowercase letters? [y/n]")
+    // Depending on the response do different things.
     switch (lowerPool){
         case 'y': characterPool +=lower;return;
         case 'n': return
-        alert('Enter valid option');
+        default: alert('Enter valid option');
         lowerPool();
 
     }
 }
 function upperPool(){
-    upperPool = prompt ("Do you want uppercase letters? [y/n]")
+    var upperPool = prompt ("Do you want uppercase letters? [y/n]")
     switch (upperPool){
         case 'y': characterPool +=upper;return;
         case 'n': return
-        alert('Enter valid option');
+        default: alert('Enter valid option');
         upperPool();
 
     }
 }
 
 function numberPool(){
-    numberPool = prompt ("Do you want a number? [y/n]")
+    var numberPool = prompt ("Do you want a number? [y/n]")
     switch (numberPool){
         case 'y': characterPool +=number;return;
         case 'n': return
-        alert('Enter valid option');
+        default: alert('Enter valid option');
         numberPool();
 
     }
 }
 
 function symbolPool(){
-    symbolPool = prompt ("Do you want a symbol? [y/n]")
+    var symbolPool = prompt ("Do you want a symbol? [y/n]")
     switch (symbolPool){
         case 'y': characterPool +=symbol;return;
         case 'n': return
-        alert('Enter valid option');
+        default: alert('Enter valid option');
         symbolPool();
 
     }
@@ -94,10 +112,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
